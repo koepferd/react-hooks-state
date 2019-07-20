dbb70d59ad5bd20c73bf7daf55adb8912da4d62f
Deborah Köpfer <deborah.koepfer@virtual-identity.com>
2019-07-20 15:31:05 +0200

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ContextProvider } from './globalState/state';
 import { App } from './App';
-import { StoveTempProvider } from './globalState/StoveTempProvider';
-import { WaterStatusProvider } from './globalState/WaterStatusProvider';
-import { ChefStatusProvider } from './globalState/ChefStatusProvider';
 
 export const AppContainer = () => {
     return (
